3936b39b2fcfba0edf0120ab15a94df3490f00fc
BadalovSanan <sbadalov20576@ada.edu.az>
2025-02-05 14:26:26 +0400

    Added School.java

---
+++ School.java
@@ -0,0 +1,13 @@
+public class School {
+    String schoolMajor;
+    String location;
+    int tuitionFees;
+    boolean isActive;
+
+    public School(String schoolMajor, String location, int tuitionFees, boolean isActive) {
+        this.schoolMajor = schoolMajor;
+        this.location = location;
+        this.tuitionFees = tuitionFees;
+        this.isActive = isActive;
+    }
+}
